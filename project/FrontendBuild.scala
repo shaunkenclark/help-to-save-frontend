b5c0818281f04beee540954b6c066e8d76d79c4b
Andy Dwelly <andydwelly@gmail.com>
2017-08-10 15:07:52 +0100

    Updated library version.

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -10,7 +10,7 @@ object FrontendBuild extends Build with MicroService {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.22.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.26.0",
     "uk.gov.hmrc" %% "play-partials" % "5.3.0",
     "uk.gov.hmrc" %% "play-config" % "4.3.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
